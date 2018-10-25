a0bcf3ba8c5e39deb3373da04d31fa6cc4a2ade1
Kartheek Javvaji <kjavvaji@tekion.com>
2018-10-25 19:42:18 +0530

    Change public path in webpack

--- webpack.prod.config.js
+++ webpack.prod.config.js
@@ -12,29 +12,29 @@ module.exports = {
     new webpack.HashedModuleIdsPlugin() // so that file hashes don't change unexpectedly
   ],
   output: {
-    publicPath: '/',
+    publicPath: '',
     path: path.resolve(__dirname, 'docs'),
     filename: '[name].[hash].js'
-  }
-  // optimization: {
-  //   runtimeChunk: 'single',
-  //   splitChunks: {
-  //     chunks: 'all',
-  //     maxInitialRequests: Infinity,
-  //     minSize: 0,
-  //     cacheGroups: {
-  //       vendor: {
-  //         test: /[\\/]node_modules[\\/]/,
-  //         name (module) {
-  //           // get the name. E.g. node_modules/packageName/not/this/part.js
-  //           // or node_modules/packageName
-  //           const packageName = module.context.match(/[\\/]node_modules[\\/](.*?)([\\/]|$)/)[1]
+  },
+  optimization: {
+    runtimeChunk: 'single',
+    splitChunks: {
+      chunks: 'all',
+      maxInitialRequests: Infinity,
+      minSize: 0,
+      cacheGroups: {
+        vendor: {
+          test: /[\\/]node_modules[\\/]/,
+          name (module) {
+            // get the name. E.g. node_modules/packageName/not/this/part.js
+            // or node_modules/packageName
+            const packageName = module.context.match(/[\\/]node_modules[\\/](.*?)([\\/]|$)/)[1]
 
-  //           // npm package names are URL-safe, but some servers don't like @ symbols like .Net
-  //           return `npm.${packageName.replace('@', '')}`
-  //         }
-  //       }
-  //     }
-  //   }
-  // }
+            // npm package names are URL-safe, but some servers don't like @ symbols like .Net
+            return `npm.${packageName.replace('@', '')}`
+          }
+        }
+      }
+    }
+  }
 }
